f38403205c7ab14f6331f483075e37b65ced4261
Wildan Kusnaedi <Wildank40@gmail.com>
2023-10-03 22:58:01 +0700

    Add Image Picker

--- lib/widgets/dialog_add_meat.dart
+++ lib/widgets/dialog_add_meat.dart
@@ -1,15 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:meat_list/data/app_style.dart';
 
 import '../data/meat.dart';
 
-class AddDialog extends StatelessWidget {
+class AddDialog extends StatefulWidget {
   const AddDialog({
     super.key,
     this.meat,
   });
 
   final Meat? meat;
+
+  @override
+  State<AddDialog> createState() => _AddDialogState();
+}
+
+class _AddDialogState extends State<AddDialog> {
+  String imagePath = '';
+  File? image;
+
+  Future<void> uploadImage() async {
+    var result = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (result != null) {
+        image = File(result.path);
+        imagePath = result.name;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     String name = '';
@@ -18,10 +40,10 @@ class AddDialog extends StatelessWidget {
     String title = 'Add Meat';
     bool enabled = true;
 
-    if (meat != null) {
-      name = meat!.name;
-      image = meat!.image;
-      desc = meat!.description;
+    if (widget.meat != null) {
+      name = widget.meat!.name;
+      image = widget.meat!.image;
+      desc = widget.meat!.description;
       title = 'Edit Meat';
       enabled = false;
     }
@@ -49,14 +71,29 @@ class AddDialog extends StatelessWidget {
             TextField(
                 controller: TextEditingController(text: desc),
                 onChanged: (value) => desc = value,
-                decoration: AppStyles.hintText(Icons.description, 'Desc'))
+                decoration: AppStyles.hintText(Icons.description, 'Desc')),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                    onPressed: uploadImage, child: const Text('Pick Image')),
+                if (imagePath.isNotEmpty)
+                  const Icon(Icons.check_circle)
+                else
+                  const Icon(Icons.check_circle_outline),
+              ],
+            )
           ],
         ),
       ),
       actions: [
         TextButton(
             onPressed: () {
-              if (name.isEmpty || image.isEmpty || desc.isEmpty) {
+              if (name.isEmpty ||
+                  image.isEmpty ||
+                  desc.isEmpty ||
+                  imagePath.isEmpty) {
                 AppStyles.toastMessage('Fill all the Input');
               } else {
                 Navigator.of(context).pop([name, image, desc]);
