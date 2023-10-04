2bd198c4dd19e397c5dba3f428705146b514ed78
Wildan Kusnaedi <Wildank40@gmail.com>
2023-10-04 21:33:25 +0700

    Remove unused class

--- lib/data/meat.dart
+++ lib/data/meat.dart
@@ -20,28 +20,3 @@ class Meat {
     };
   }
 }
-
-class MeatOperation {
-  static final MeatOperation _meatOperation = MeatOperation._internal();
-  factory MeatOperation() {
-    return _meatOperation;
-  }
-  MeatOperation._internal();
-  var meatList = [];
-
-  void addMeat(Meat meat) {
-    meatList.add(meat);
-  }
-
-  void removeMeat(Meat meat) {
-    meatList.remove(meat);
-  }
-
-  void editMeat(Meat meat, int index) {
-    meatList[index] = meat;
-  }
-
-  List getMeat() {
-    return meatList;
-  }
-}
